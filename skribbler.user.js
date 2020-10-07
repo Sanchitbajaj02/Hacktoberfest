aa76a196a3ddcb3d64dd0661eb3c4fa099916b2e
mandar1jn <49076509+mandar1jn@users.noreply.github.com>
2020-10-07 09:49:51 +0200

    Update skribbler.user.js
    to make the issue and homepage url reflect this repository

--- skribbler.user.js
+++ skribbler.user.js
@@ -3,14 +3,14 @@
 // @connect skribbler.herokuapp.com
 // @grant GM_xmlhttpRequest
 // @grant GM.xmlHttpRequest
-// @homepageURL https://github.com/rosslh/skribbler
+// @homepageURL https://github.com/Sanchitbajaj02/Hacktoberfest
 // @icon https://skribbl.io/res/favicon.png
 // @licence MIT
 // @match *://skribbl.io/*
 // @name Skribbler
 // @namespace https://rosshill.ca
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @supportURL https://github.com/rosslh/skribbler/issues
+// @supportURL https://github.com/Sanchitbajaj02/Hacktoberfest/issues
 // @version 2.6.3
 // ==/UserScript==
 const state = {
